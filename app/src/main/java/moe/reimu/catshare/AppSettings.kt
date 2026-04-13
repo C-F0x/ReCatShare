116911b58db620cab6f957256a7c33e836f02e70
C-F0x <SuperSU@outlook.sg>
2026-04-13 16:37:23 +0800

    upgrade and better devicename

--- app/src/main/java/moe/reimu/catshare/AppSettings.kt
+++ app/src/main/java/moe/reimu/catshare/AppSettings.kt
@@ -3,15 +3,15 @@ package moe.reimu.catshare
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.core.content.edit
+import moe.reimu.catshare.utils.DeviceName
 
 class AppSettings(private val context: Context) {
     private val prefs: SharedPreferences = context.getSharedPreferences("app", Context.MODE_PRIVATE)
 
     var deviceName: String
-        get() = prefs.getString(
-            "deviceName",
-            context.getString(R.string.device_name_default_value)
-        )!!
+        get() = prefs.getString("deviceName", null)
+            ?: DeviceName.get().takeUnless { it.isBlank() }
+            ?: context.getString(R.string.device_name_default_value)
         set(value) {
             prefs.edit { putString("deviceName", value) }
         }
